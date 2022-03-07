7eff79e079623543c05ef9bacb9c4ec7db4895a3
konstantinos mazarakis <kostasbusiness@gmail.com>
2022-03-07 10:31:30 -0500

    commit

--- watchlist.py
+++ watchlist.py
@@ -1,6 +1,8 @@
+from operator import index
 import pandas as pd # Allows for further data manipulation and analysis
 from pandas_datareader import data as web # Reads stock data 
 import datetime as dt # For defining dates
+import numpy as np
 import os
 tickers=pd.read_csv("tickers.csv")
 
@@ -39,12 +41,12 @@ def get_df_from_csv(ticker,file = "single"):
     else:
         return df
 
-
+# Finds the Coefficient of variation from a stock on specific days
 def coefficient_of_variation(stock):
     cov = round((stock['Close'].std() / stock['Close'].mean() * 100),2)
     return cov
 
-
+#Finds the return of invesment from a stock on specific days
 def roi(stock):
     try:
         start_val = float(stock.head(1)["Close"])
@@ -58,49 +60,44 @@ def roi(stock):
         return roi
 
 
-def watchlist():
+
+
+def watchlist(num = 20):
+    cols = {
+        "Tickers":[""],
+        "Price":[""],
+        "Cov":[""],
+        "Roi":[""],
+        "Volume":[""]
+    }
+    top_20 = pd.DataFrame(cols)
     all_stacks_folder = os.scandir('C:/CodingDojo/projects/watchlist/csv/all/')
+
     for ticker in all_stacks_folder:
         try:
             stock = pd.read_csv(ticker)
             cov = coefficient_of_variation(stock)
             roi_results = roi(stock)
-            print(f"{ticker} : Cov : {cov}      Roi : {roi_results}")
+            top_20 = pd.DataFrame(np.insert(top_20.values,1,[ticker,round(float(stock.tail(1)["Close"]),2),cov,roi_results,int(stock.tail(1)["Volume"])],axis= 0),columns= cols)
         except TypeError:
             print("File Doesn't Exist")
+    top_20.drop([0], inplace = True)
+    top_20.to_csv('C:/CodingDojo/projects/watchlist/top_20.csv')
+    print(top_20.sort_values(by=['Roi'], ascending=False).head(num))
     return
 
 
 #RUN
 
-watchlist()
+watchlist(10)
 
 # save_to_csv_from_yahoo_all_stocks(2022,2,21,2022,2,25)
 
 
-# save_to_csv_from_yahoo("AAPL",2022,2,21,2022,2,25)
-# AAPL =get_df_from_csv("AAPL")
+# save_to_csv_from_yahoo("BTC",2022,2,21,2022,2,25)
+# AAPL =get_df_from_csv("BTC")
 # print(coefficient_of_variation(AAPL))
 # print(roi(AAPL))
-
-
-
-
-
-#OLD WAY
-
-# def watchlist():
-#     for ticker in tickers['Symbol']:
-#         try:
-#             stock = get_df_from_csv(str(ticker), file = "all")
-#             cov = coefficient_of_variation(stock)
-#             roi_results = roi(stock)
-#             print(f"{ticker} : Cov : {cov}      Roi : {roi_results}")
-#         except TypeError:
-#             print("File Doesn't Exist")
-#     return
-
-
 
 
 
